b57299259d5cc7e70390f9a6748b8b1946199cf9
SimonDMC <simondmcplayer@gmail.com>
2025-08-04 17:51:03 +0200

    init

---
+++ resources/views/layout/page.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <link rel="preconnect" href="https://rsms.me/">
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+        @vite(['resources/js/app.js'])
+    </head>
+    <body>
+        @yield('page')
+    </body>
+</html>
---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.page')
+
+@section('title')
+    Wormhole
+@endsection
+
+@section('page')
+    <h1>Wormhole</h1>
+@endsection
