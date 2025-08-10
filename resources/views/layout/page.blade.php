9b5e4b6d13a24f2e8ee94559a469a60acc7fce85
SimonDMC <simondmcplayer@gmail.com>
2025-08-10 21:48:10 +0200

    Add icons and manifest

--- resources/views/layout/page.blade.php
+++ resources/views/layout/page.blade.php
@@ -3,11 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="/icon.png" />
+	    <link rel="apple-touch-icon" href="/maskable.png" />
 
         <title>Wormhole</title>
 
         <link rel="preconnect" href="https://rsms.me/">
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
+        <meta content="Wormhole" property="og:title" />
+        <meta content="Transfer files between devices with ease" property="og:description" />
+        <meta content="Transfer files between devices with ease" name="description">
+        <meta content="https://wormhole.simondmc.com" property="og:url" />
+        <meta content="#1c1c1d" data-react-helmet="true" name="theme-color" />
+        <link rel="manifest" href="/manifest.json">
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="vapid-public-key" content="{{ env('VAPID_PUBLIC_KEY') }}">
